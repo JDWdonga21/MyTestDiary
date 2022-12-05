84387a2ba23b5f7fd0a55c912677abbd90651d05
jdwdonga21 <ongame21212144@gmail.com>
2022-12-05 14:27:25 +0900

    main 화면 구성 - FloatingActionButton - new -> vectorAsset - FloatingActionButton floatingActionButton = findViewById(R.id.writeBtn) (xml에서 작성한 버튼 가져오기) - setOnClickListener(new View... (버튼 기능 구현)

--- app/src/main/java/com/kuderitest/mytestdiary/MainActivity.java
+++ app/src/main/java/com/kuderitest/mytestdiary/MainActivity.java
@@ -3,6 +3,9 @@ package com.kuderitest.mytestdiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.writeBtn);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //작성하기 버튼 클릭시 호출되는 곳
+
+            }
+        });
     }
 }
